c9cedaa5fcfd7e84cf568a93856a468e208869b3
im-zero57 <leman000@sookmyung.ac.kr>
2020-10-31 23:43:13 +0900

    practice2

--- week3/dut_practice1.v
+++ week3/dut_practice1.v
@@ -47,3 +47,85 @@ always @(*) begin
 end
 
 endmodule
+
+//--------------------------------
+// practice 02 D latch
+//--------------------------------
+
+module	d_latch(	q,
+			d,
+			clk,
+			rst_n)	;
+output		q	;
+input		d	;
+input		clk	;
+input		rst_n	;
+
+reg		q	;
+
+always@(d or clk or rst_n) begin
+	if(rst_n == 1'b0) begin
+		q <= 1'b0	;
+	end else begin
+		if(clk == 1'b1) begin
+			q <= d	;
+		end
+	end
+end
+
+endmodule
+
+//--------------------------------
+// D Flop-Flop/ async & sync
+//--------------------------------
+
+module	dff_asyn(	q,
+			d,
+			clk,
+			rst_n	);
+
+output			q	;
+input			d	;
+input			clk	;
+input			rst_n	;
+
+reg			q	;
+always @( posedge clk or negedge rst_n	) begin
+	if(rst_n == 1'b0) begin
+		q	<= 1'b0		;
+	end else begin
+		q	<= d		;
+	end
+
+end
+
+endmodule
+
+//--------
+//sync
+//-------
+
+module dff_syn(	q,
+		d,
+		clk,
+		rst_n);
+
+output		q	;
+input		d	;
+input		clk	;
+input		rst_n	;
+
+reg		q	;
+
+always @( posedge clk) begin
+	if(rst_n == 1'b0) begin
+		q <= 1'b0	;
+	end else begin
+		q <= d		;
+	end
+
+end
+
+endmodule
+
+
--- week3/tb_practice1.v
+++ week3/tb_practice1.v
@@ -32,3 +32,61 @@ initial begin
 #(10)	in = 3'b111	;
 
 #(10) $finish		;
+
+end
+
+endmodule
+//--------------------------------
+// practice 02
+//--------------------------------
+
+module	tb_sequential	;
+
+wire	q_latch		;
+wire	q_dff_asyn	;
+wire	q_dff_syn	;
+
+reg	d		;
+reg	clk		;
+reg	rst_n		;
+
+initial		clk = 1'b0	;
+always	#(100)	clk = ~clk	;
+
+//------------------
+// instances
+//------------------
+d_latch		dut0(	.q	( q_latch	),
+			.d	( d		),
+			.clk	( clk		),
+			.rst_n	( rst_n		));
+
+dff_asyn	dut1(	.q	( q_dff_asyn	),
+			.d	( d		),
+			.clk	( clk		),
+			.rst_n	( rst_n		));
+
+dff_syn		dut2(	.q	( q_dff_syn	),
+			.d	( d		),
+			.clk	( clk		),
+			.rst_n	( rst_n		));
+
+//----------
+//stimulus
+//----------
+
+initial begin
+$display("===========================================")	;
+$display("	rst_n	d	q0	q1	q2")	;
+$display("===========================================")	;
+#(0)	{rst_n, d} = 2'b00;
+#(50)	{rst_n, d} = 2'b00;	#(50)	$display("	%d	%d	%d	%d	%d", rst_n,d,q_latch,q_dff_asyn,q_dff_syn);
+#(50)	{rst_n, d} = 2'b10;	#(50)	$display("	%d	%d	%d	%d	%d", rst_n,d,q_latch,q_dff_asyn,q_dff_syn);
+#(50)	{rst_n, d} = 2'b11;	#(50)	$display("	%d	%d	%d	%d	%d", rst_n,d,q_latch,q_dff_asyn,q_dff_syn);
+#(50)	{rst_n, d} = 2'b10;	#(50)	$display("	%d	%d	%d	%d	%d", rst_n,d,q_latch,q_dff_asyn,q_dff_syn);
+#(50)	{rst_n, d} = 2'b11;	#(50)	$display("	%d	%d	%d	%d	%d", rst_n,d,q_latch,q_dff_asyn,q_dff_syn);
+#(50)	{rst_n, d} = 2'b10;	#(50)	$display("	%d	%d	%d	%d	%d", rst_n,d,q_latch,q_dff_asyn,q_dff_syn);
+#(50)	$finish;	
+end
+
+endmodule
