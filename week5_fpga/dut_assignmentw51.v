89d0f9ec7c567840720a5de1b983524b12f02acc
im-zero57 <leman000@sookmyung.ac.kr>
2020-11-07 20:46:44 +0900

    assignment1

--- week5_fpga/dut_assignmentw51.v
+++ week5_fpga/dut_assignmentw51.v
@@ -49,10 +49,10 @@ always @(posedge clk or negedge rst_n) begin
 	if(rst_n == 1'b0) begin
 		out <= 6'd0	;
 	end else begin
-		if(out >= 6'd59) begin
-			out <= 6'd0	;
+		if(out <= 6'd0) begin
+			out <= 6'd59	;
 		end else begin
-			out <= out+1'b1	;
+			out <= out-1'b1	;
 		end
 	end
 end
